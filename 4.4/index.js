24b9f4befe640aab2d0f151c012f57d2a6e11aa8
kennycha <dudqn136@naver.com>
2021-08-09 15:55:55 +0900

    add buffer setting functions for floor and cube

--- 4.4/index.js
+++ 4.4/index.js
@@ -108,6 +108,187 @@ const popModelViewMatrix = () => {
   modelViewMatrix = modelViewMatrixStack.pop();
 };
 
+const setupFloorBuffers = () => {
+  floorVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, floorVertexPositionBuffer);
+  const floorVertextPosition = [
+    5.0,
+    0.0,
+    5.0,
+    5.0,
+    0.0,
+    -5.0,
+    -5.0,
+    0.0,
+    -5.0,
+    -5.0,
+    0.0,
+    5.0,
+  ];
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(floorVertextPosition),
+    gl.STATIC_DRAW
+  );
+  floorVertexPositionBuffer.itemSize = 3;
+  floorVertexPositionBuffer.numberOfItems = 4;
+
+  floorVertexIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, floorVertexIndexBuffer);
+  const floorVertexIndices = [0, 1, 2, 3];
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(floorVertexIndices),
+    gl.STATIC_DRAW
+  );
+  floorVertexIndexBuffer.itemSize = 1;
+  floorVertexIndexBuffer.numberOfItems = 4;
+};
+
+const setupCubeBuffers = () => {
+  cubeVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+  const cubeVertexPosition = [
+    1.0,
+    1.0,
+    1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    1.0,
+
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    1.0,
+    -1.0,
+    -1.0,
+
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    1.0,
+
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    1.0,
+    1.0,
+
+    1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+    1.0,
+  ];
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(cubeVertexPosition),
+    gl.STATIC_DRAW
+  );
+  cubeVertexPositionBuffer.itemSize = 3;
+  cubeVertexPositionBuffer.numberOfItems = 24;
+
+  cubeVertexIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+  const cubeVertexIndices = [
+    0,
+    1,
+    2,
+    0,
+    2,
+    3,
+    4,
+    6,
+    5,
+    4,
+    7,
+    6,
+    8,
+    9,
+    10,
+    8,
+    10,
+    11,
+    12,
+    13,
+    14,
+    12,
+    14,
+    15,
+    16,
+    17,
+    18,
+    16,
+    18,
+    19,
+    20,
+    22,
+    21,
+    20,
+    23,
+    22,
+  ];
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(cubeVertexIndices),
+    gl.STATIC_DRAW
+  );
+  cubeVertexIndexBuffer.itemSize = 1;
+  cubeVertexIndexBuffer.numberOfItems = 36;
+};
+
+const setupBuffers = () => {
+  setupFloorBuffers();
+  setupCubeBuffers();
+};
+
 const startup = () => {
   canvas = document.getElementById("myGLCanvas");
   gl = createGLContext(canvas);
